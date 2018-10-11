2247b26ddd269e802a6228e6b1dd6670987eef69
student <student@DEV1-12>
2018-10-11 10:29:55 +0900

    主人公君追加

---
+++ EscapeGame/EscapeGame/ObjHero.h
@@ -0,0 +1,26 @@
+#pragma once
+//使用するヘッダー
+#include"GameL\SceneObjManager.h"
+//使用するネームスペース
+using namespace GameL;
+
+//オブジェクト：主人公
+class CObjHero : public CObj
+{
+   public:
+	   CObjHero() {};
+	   ~CObjHero() {};
+	   void Init();    //イニシャライズ
+	   void Action();  //アクション
+	   void Draw();    //ドロー
+  private:
+	  float m_px; //位置
+	  float m_py;
+	  float m_vx; //移動ベクトル
+	  float m_vy;
+	  float m_posture; //姿勢
+	  float m_speed; //歩く速度
+
+	  int m_ani_time;
+	  int m_ani_frame;
+};
