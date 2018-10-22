3cf0a017c6ab1044016f416a5db0623c828fb5a6
student <student@DEV1-12>
2018-10-22 11:09:19 +0900

    a

--- EscapeGame/EscapeGame/ObjHero.h
+++ EscapeGame/EscapeGame/ObjHero.h
@@ -13,7 +13,23 @@ class CObjHero : public CObj
 	   void Init();    //イニシャライズ
 	   void Action();  //アクション
 	   void Draw();    //ドロー
-  private:
+
+	   float GetX() { return m_px; }
+	   float GetY() { return m_px; }
+	   float GetVX() { return m_vx; }
+	   float GetVY() { return m_vy; }
+
+	   void SetX(float x) { m_px = x; }
+	   void SetY(float y) { m_py = y; }
+	   void SetVX(float vx) { m_vx = vx; }
+	   void SetVY(float vy) { m_vy = vy; }
+
+
+	   void SetUp(bool b) { m_hit_up = b; }
+	   void SetDown(bool b) { m_hit_down = b; }
+	   void SetLeft(bool b) { m_hit_left = b; }
+	   void SetRight(bool b) { m_hit_right = b; }
+   private:
 	  float m_px; //位置
 	  float m_py;
 	  float m_vx; //移動ベクトル
@@ -23,4 +39,10 @@ class CObjHero : public CObj
 
 	  int m_ani_time;
 	  int m_ani_frame;
+
+	  //blockとの衝突状態確認用
+	  bool m_hit_up;
+	  bool m_hit_down;
+	  bool m_hit_left;
+	  bool m_hit_right;
 };
