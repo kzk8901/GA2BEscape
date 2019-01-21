1027d5a18d33469cc2ab0b8f733749689843fa18
student <student@DEV1-12>
2019-01-21 11:12:22 +0900

    テキスト

--- EscapeGame/EscapeGame/ObjItem.cpp
+++ EscapeGame/EscapeGame/ObjItem.cpp
@@ -18,6 +18,9 @@ void CObjItem::Init()
 	{
 		((UserData*)Save::GetData())->item[i] = 0;
 	}
+	((UserData*)Save::GetData())->item[0] = 9;
+    ((UserData*)Save::GetData())->item[1] = 10;
+	((UserData*)Save::GetData())->item[2] = 11;
 }
 
 void CObjItem::Action()
