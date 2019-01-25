0674d052ef0bdbe8b95f3ed39c431c7563d4b80c
GA2 <GA2@DEV1-11>
2019-01-25 16:08:25 +0900

    デ☆デ☆チ☆テ☆ー

--- EscapeGame/EscapeGame/ObjBlock.cpp
+++ EscapeGame/EscapeGame/ObjBlock.cpp
@@ -1003,6 +1003,7 @@ void CObjBlock::Action()
 		(mouse_x == kirara_x && mouse_y - 1 == kirara_y))
 	{
 		cought = true;
+		itm->GetItem(12);
 	}
 	while (mouseplo == false && mapnum == mousemap && cought == false)
 	{
